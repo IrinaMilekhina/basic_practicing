e5c9472403e3bb7c56410a1e43eb955e27a8466d
thetribalme <thetribaleng@gmail.com>
2024-08-13 19:37:59 +0200

    visualization through print added

--- comeback/4.py
+++ comeback/4.py
@@ -6,7 +6,9 @@ from func import math_wizard
 
 # list generation
 initial_list = math_wizard(10, 0, 10)
+print(initial_list)
 # list transformation
 for element in initial_list:
     if element < initial_list[-1]:
         initial_list[initial_list.index(element)] = initial_list[-1]
+print(initial_list)
