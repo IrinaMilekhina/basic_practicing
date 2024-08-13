ec2bbac96ffc74a62dbbebc9b6881bf51b5e9432
thetribalme <thetribaleng@gmail.com>
2024-08-13 19:38:40 +0200

    task solved

--- comeback/5.py
+++ comeback/5.py
@@ -3,3 +3,11 @@
 Удалить в массиве все числа, которые повторяются более двух раз.
 Результат записать в txt файл.
 """
+from func import math_wizard
+
+# list generation
+initial_list = math_wizard(10, 0, 10)
+print(initial_list)
+# list transformation
+initial_list = list(set(initial_list))
+print(initial_list)
