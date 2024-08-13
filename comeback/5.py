fa33d8f5166acdd0354e54013ef56df91f3b657b
thetribalme <thetribaleng@gmail.com>
2024-08-13 20:06:44 +0200

    txt file was writen down

--- comeback/5.py
+++ comeback/5.py
@@ -5,9 +5,13 @@
 """
 from func import math_wizard
 
+file_name = 'altered_list.txt'
 # list generation
 initial_list = math_wizard(10, 0, 10)
 print(initial_list)
 # list transformation
 initial_list = list(set(initial_list))
 print(initial_list)
+# writing down the file
+with open(file_name, 'w', encoding='utf-8') as file:
+    file.write(str(initial_list))
