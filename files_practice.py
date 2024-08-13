d39c1fbc7b9a1623565384bdbc9373802b46ce55
thetribalme <thetribaleng@gmail.com>
2024-08-13 14:32:26 +0200

    task description added

--- files_practice.py
+++ files_practice.py
@@ -13,6 +13,3 @@
 - обработка новых файлов
 - удаление неактуальных файлов (json есть, а txt нет)
 """
-import os
-
-print(os.listdir(r'/Users/irinamilekhina/python_practice/teaching/basic_practicing'))
