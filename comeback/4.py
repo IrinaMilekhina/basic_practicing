5feacda10a204bd319682b1a19273b112b0f2ca7
thetribalme <thetribaleng@gmail.com>
2024-08-13 19:30:37 +0200

    task solved

--- comeback/4.py
+++ comeback/4.py
@@ -2,3 +2,11 @@
 Использовать функцию из 3 задания на вход.
 Заменить все числа, меньшие последнего элемента массива, на первый элемент.
 """
+from func import math_wizard
+
+# list generation
+initial_list = math_wizard(10, 0, 10)
+# list transformation
+for element in initial_list:
+    if element < initial_list[-1]:
+        initial_list[initial_list.index(element)] = initial_list[-1]
